82db7f64671ddc353a6a012e82a837d294bef930
Zibing Zhang <zibing.zha@gmail.com>
2023-12-22 11:37:43 -0400

    Save meets

--- registration/urls.py
+++ registration/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("athlete", views.athlete, name="register athlete"),
+    path("athlete", views.manage_athletes, name="register athlete"),
     path("meet/<int:meet_id>/team/<int:team_id>/", views.meet_entry_form, name="meet signup"),
     path("meet/<int:meet_id>/team/<int:team_id>/save/", views.save_meet_entry_form, name="meet signup save"),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,48 +1,70 @@
 from collections import defaultdict
+from decimal import Decimal
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from registration.models import Athlete
+from registration.models import Athlete, Meet
 from common.constants import INDIVIDUAL_EVENTS, EVENT_ORDER
+from django.views.decorators.http import require_http_methods
 from registration.forms import MeetAthleteRelayEntryForm, MeetAthleteIndividualEntryForm, AthleteForm
 from registration.models import MeetAthleteIndividualEntry
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+@login_required
+@require_http_methods(["GET"])
+def manage_athletes(request):
+    return render(request, "athletes.html", {"form": AthleteForm(), "athletes": Athlete.objects.filter()})
 
 
 @login_required
+@require_http_methods(["GET"])
 def meet_entry_form(request, meet_id, team_id):
-    if request.method == "GET":
-        sections = []
-        entries = MeetAthleteIndividualEntry.objects.filter(meet_id=meet_id, athlete__team_id=team_id)
-        entries_by_event = defaultdict(lambda: [])
-        for entry in entries:
-            entries_by_event[entry.event].append(entry)
-        for event in EVENT_ORDER:
-            if event in INDIVIDUAL_EVENTS:
-                forms = []
-                for i in range(4):
-                    try:
-                        forms.append(MeetAthleteIndividualEntryForm(team_id, prefix=f"{event.as_prefix()}-{i}", initial={"athlete": entries_by_event[event][i].athlete_id, "seed": entries_by_event[event][i].seed}))
-                    except IndexError:
-                        forms.append(MeetAthleteIndividualEntryForm(team_id, prefix=f"{event.as_prefix()}-{i}"))
-                sections.append({
-                    "event": event.value,
-                    "forms": forms
-                })
-        return render(request, "meet_entry.html", {"sections": sections})
+    sections = []
+    entries = MeetAthleteIndividualEntry.objects.filter(meet_id=meet_id, athlete__team_id=team_id)
+    entries_by_event = defaultdict(lambda: [])
+    for entry in entries:
+        entries_by_event[entry.event].append(entry)
+    for event in EVENT_ORDER:
+        if event in INDIVIDUAL_EVENTS:
+            forms = []
+            for i in range(4):
+                try:
+                    forms.append(MeetAthleteIndividualEntryForm(team_id, prefix=f"{event.as_prefix()}-{i}", initial={"athlete": entries_by_event[event][i].athlete_id, "seed": entries_by_event[event][i].seed}))
+                except IndexError:
+                    forms.append(MeetAthleteIndividualEntryForm(team_id, prefix=f"{event.as_prefix()}-{i}"))
+            sections.append({
+                "event": event.value,
+                "forms": forms
+            })
+    return render(request, "meet_entry.html", {"sections": sections})
 
 
 @login_required
+@require_http_methods(["POST"])
 def save_meet_entry_form(request, meet_id, team_id):
-    if request.method == "POST":
-        print(request._post)
-        return HttpResponse(200)
+    entries = MeetAthleteIndividualEntry.objects.filter(meet_id=meet_id, athlete__team_id=team_id)
+    entries_by_event_athlete_id = {}
+    for entry in entries:
+        entries_by_event_athlete_id[(entry.event, str(entry.athlete.id))] = entry
+    for event in INDIVIDUAL_EVENTS:
+        for i in range(4):
+            athlete_id = request.POST[f"{event.as_prefix()}-{i}-athlete"]
+            seed = request.POST[f"{event.as_prefix()}-{i}-seed"]
+            if athlete_id == "" or seed == "":
+                # TODO: validation error
+                continue
+            entry = entries_by_event_athlete_id.get((event, athlete_id))
+            if entry:
+                entry.seed = Decimal(seed)
+                entry.save()
+                del entries_by_event_athlete_id[(event, athlete_id)]
+            else:
+                if seed:
+                    MeetAthleteIndividualEntry.objects.create(meet_id=meet_id, athlete_id=int(athlete_id), event=event, seed=Decimal(seed))
+                else:
+                    MeetAthleteIndividualEntry.objects.create(meet_id=meet_id, athlete_id=int(athlete_id), event=event)
 
-
-@login_required
-def athlete(request):
-    if request.method == "GET":
-        return render(request, "athletes.html", {"form": AthleteForm(), "athletes": Athlete.objects.filter()})
+    for entry in entries_by_event_athlete_id.values():
+        entry.delete()
+    return HttpResponse()
