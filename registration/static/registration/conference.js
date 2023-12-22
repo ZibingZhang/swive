477b20fd1c1d686ded252a47428a05e768aa6e42
Zibing Zhang <zibing.zha@gmail.com>
2023-12-21 22:11:54 -0400

    Clean up script

--- registration/static/registration/conference.js
+++ registration/static/registration/conference.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var form = document.getElementById("myForm");
+  const form = document.getElementById("myForm");
   function handleForm(event) {
     event.preventDefault();
-    var data = new FormData(form);
-    console.log(location.pathname);
-    var match = location.pathname.match(/registration\/meet\/(\d+)\/team\/(\d+)\//);
-    var meetId = match[1];
-    var teamId = match[2];
+    const match = location.pathname.match(/registration\/meet\/(\d+)\/team\/(\d+)\//);
+    const meetId = match[1];
+    const teamId = match[2];
     fetch(window.location.origin + `/registration/meet/${meetId}/team/${teamId}/save/`, {
       method: 'POST',
         body: new FormData(event.target),
@@ -15,9 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
           return response.json();
         }
         return Promise.reject(response);
-      }).then(function (data) {
-        console.log(data);
-      }).catch(function (error) {
+      }).catch(error => {
         console.warn(error);
       });
   }
